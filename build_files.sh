0ee54cf83707f6b4d3ffaaa31bafdb6f5c03f7c0
Amey <kubetkarpratham14@gmail.com>
2026-03-03 21:55:12 +0530

    Deployment file fixes

--- build_files.sh
+++ build_files.sh
@@ -7,15 +7,15 @@ echo "Build Started..."
 
 # 1. Install Dependencies
 echo "Installing Dependencies..."
-python3.9 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # 2. Collect Static Files
 echo "Collecting Static Files..."
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # 3. Run Migrations (Optional but recommended for auto-updates)
 echo "Running Migrations..."
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 echo "Build Completed Successfully!"
